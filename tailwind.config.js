3f9d91312a60740b87cfc4eb5ed559d1500a796b
PRATHID08 <Prathikshadharnappa@gmail.com>
2024-09-16 21:35:34 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Includes all files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
